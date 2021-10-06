85be214a703915577fe274fae6650f7e9fc34427
gabriella garcia <gabirvgarcia@hotmail.com>
2021-10-05 22:20:22 -0300

    starting to link answers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,15 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @answers = [ 'Great!', 'Silly question, get dressed and go to work!', "I don't care, get dressed and go to work!" ]
 
+    if params[:answer]
+      @answers = @answers.select { |answer| answer.start_with?(params[:answer]) }
+    end
   end
+
 end
+
+# If the message is I am going to work, the coach will answer Great! %>
+# If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!. %>
+# Otherwise the coach will answer I don't care, get dressed and go to work! %>
